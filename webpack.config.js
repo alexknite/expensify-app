a18948ba4d14fad93e003b00cbb8ba55a9696472
DevRice <alexniteo@gmail.com>
2018-03-30 14:17:02 -0700

    Final fix for database test env

--- webpack.config.js
+++ webpack.config.js
@@ -52,8 +52,8 @@ module.exports = (env) => {
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
         'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STOREAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STOREAGE_BUCKET)
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_STOREAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STOREAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
