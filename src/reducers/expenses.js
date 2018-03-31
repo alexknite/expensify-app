c40339970d4f7e23d14442d4c5f19ceb709de9a1
DevRice <alexniteo@gmail.com>
2018-03-30 20:10:48 -0700

    Get firebase expenses on start

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -20,6 +20,8 @@ export default (state = expensesReducerDefState, action) => {
           return expense;
         }
       });
+    case 'SET_EXPENSES':
+      return action.expenses;
     default:
       return state;
   }
--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -3,19 +3,19 @@ import moment from 'moment';
 export default [{
   id: '1',
   description: 'Gum',
-  notes: '',
+  note: '',
   amount: 195,
   createdAt: 0
 }, {
   id: '2',
   description: 'Rent',
-  notes: '',
+  note: '',
   amount: 1109500,
   createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
   id: '3',
   description: 'Credit Card',
-  notes: '',
+  note: '',
   amount: 4500,
   createdAt: moment(0).add(4, 'days').valueOf()
 }];
